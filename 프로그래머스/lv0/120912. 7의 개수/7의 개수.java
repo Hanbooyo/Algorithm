16d28f9717e374bcab0e65b98fbe76f6c550ccee
Han Bu-Young <104435251+Hanbooyo@users.noreply.github.com>
2023-03-23 14:22:26 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i=0; i<array.length; i++) {
+            int temp=array[i];
+             while(temp != 0) {
+                 if(temp%10 == 7) {
+                     answer ++;
+                 }
+                 temp = temp/10;
+             }
+        }
+        return answer;
+    }
+}
