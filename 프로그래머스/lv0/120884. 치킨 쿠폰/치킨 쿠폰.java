6199fd0af07479a737b292a64dc10f98055178dd
Han Bu-Young <104435251+Hanbooyo@users.noreply.github.com>
2023-03-30 13:51:38 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.59 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        int coupon = 0;
+        while (chicken > 0) {
+            chicken--;
+            coupon++;
+            if (coupon == 10) {
+                answer++;
+                coupon = 0;
+                chicken++;
+            }
+        }
+        return answer;
+    }
+}
