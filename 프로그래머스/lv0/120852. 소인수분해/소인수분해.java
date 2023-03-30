341794aa35a14108824f21a0a1373b7f4a34b263
Han Bu-Young <104435251+Hanbooyo@users.noreply.github.com>
2023-03-30 12:40:13 +0900

    [level 0] Title: 소인수분해, Time: 0.25 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                set.add(i);
+                n /= i;
+            }
+        }
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
