14832700c34ee92ddaa646269cbfbfd813aa1ea0
Han Bu-Young <104435251+Hanbooyo@users.noreply.github.com>
2023-06-29 18:45:42 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,16 +1,16 @@
 function solution(array) {
     let answer = 0;
-    let index = new Array(1001).fill(0);
+    let index = new Array(1001).fill(0); // 1001 크기의 배열 생성 및 0으로 초기화
 
     for (let i = 0; i < array.length; i++) {
-        index[array[i]]++;
+        index[array[i]]++; // 배열의 값에 해당하는 인덱스의 값을 증가시킴
     }
 
     let max = 0;
     let max_index = 0;
 
     for (let i = 0; i < index.length; i++) {
-        if (index[i] > max) {
+        if (index[i] > max) { // 가장 큰 값과 그에 해당하는 인덱스를 찾음
             max = index[i];
             max_index = i;
         }
@@ -18,16 +18,16 @@ function solution(array) {
 
     let count = 0;
     for (let i = 0; i < index.length; i++) {
-        if (index[i] === max) {
+        if (index[i] === max) { // 최대값과 같은 값을 가지는 요소의 개수를 세어줌
             count++;
         }
     }
 
-    answer = max_index;
+    answer = max_index; // 결과값을 가장 큰 값의 인덱스로 설정
 
     if (count > 1) {
-        answer = -1;
+        answer = -1; // 최대값과 같은 값을 가지는 요소가 여러 개일 경우 -1로 설정
     }
 
-    return answer;
+    return answer; // 결과값 반환
 }
